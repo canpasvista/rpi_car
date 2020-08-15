38e5e84c36a5c3064f1c72246ded432b20f9a9e5
keishi kaneda <canpasvista@gmail.com>
2020-08-15 14:14:22 +0000

    commit src

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,5 +1,18 @@
+use std::error::Error;
 use tokio::time;
 use chrono::{Utc, Local, DateTime, Date};
+use rppal::gpio::Gpio;
+use libc::c_uint;
+use std::convert::TryInto;
+mod udp;
+
+use json_flex;
+use json_flex::{JFObject,Unwrap};
+
+const GPIO_L : u8 = 14;
+const GPIO_R : u8 = 15;
+
+
 
 async fn task_that_takes_a_second() {
 
@@ -10,13 +23,59 @@ async fn task_that_takes_a_second() {
     //println!("---")
 }
 
-#[tokio::main]
-async fn main() {
+fn l_motor(speed : c_uint) -> Result<(),Box<dyn Error>>{
+    let mut l_pin = Gpio::new()?.get(GPIO_L)?.into_output();
+    l_pin.set_high();
+    unsafe{ libc::usleep( speed ) }; 
+    l_pin.set_low();
+    Ok(())
+}
+fn r_motor(speed : c_uint) -> Result<(),Box<dyn Error>>{
+    let mut pin = Gpio::new()?.get(GPIO_R)?.into_output();
+    pin.set_high();
+    unsafe{ libc::usleep(speed) };
+    pin.set_low();
+    Ok(())
+}
 
-    let mut interval = time::interval(time::Duration::from_millis(100));
-    for _i in 0..5 {
+async fn setSpeed( l_speed : i64, r_speed : i64 )->Result<(), Box<dyn Error>>{
+    let mut interval = time::interval(time::Duration::from_millis(20));
+    let mut interval2 = time::interval(time::Duration::from_millis(20));
+    let mut n : i64 = l_speed;
+    let mut n2 : i64 = 0;    
+    for _i in 0..10 {
         interval.tick().await;
-        //println!("{}", Utc::now());
-        task_that_takes_a_second().await;
-    }
+	let r : u32 = (1350+l_speed-n2).try_into().unwrap();
+	r_motor(r);
+        interval2.tick().await;
+        let l : u32 = (1450-r_speed+n2).try_into().unwrap();
+        l_motor(l);
+	println!(" : {}   {}",l_speed,r_speed);
+    };
+    Ok(())
+}
+
+#[tokio::main]
+async fn main(){
+
+loop{
+  let array = udp::start();
+  match array {
+     Option::None => continue,
+     Option::Some(array)=>{
+        if *(array["stop"].unwrap_i64()) == 1 {
+           println!("stop");
+           setSpeed(0,0).await;
+        }else{
+           println!("{} - {} ", *(array["R"].unwrap_i64()), *(array["L"].unwrap_i64()));
+           let l = *(array["L"].unwrap_i64());
+           let r = *(array["R"].unwrap_i64());
+
+           setSpeed(l.try_into().unwrap(),r.try_into().unwrap()).await;
+        }
+     }
+     //setSpeed(400,400).await;
+  }
+}
+
 }
